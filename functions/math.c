c648559cc14ec1bbe45fc15ad013214dc078d12e
mondyfy <khaptadisandip@gmail.com>
2023-09-05 10:06:20 +0545

    add math functions

---
+++ functions/math.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("%.0f \n", sqrt(16));
+    printf("%.0f \n", ceil(1.4));
+    printf("%.0f \n", floor(1.4));
+    printf("%.0f \n", pow(4, 3));
+
+    return 0;
+}
